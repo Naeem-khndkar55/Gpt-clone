899e603712c5d710ad21f545547fb65c2f77c0bb
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-03-06 13:27:28 +0600

    gemini api intigration done and successfull display it to ui

--- gpt-clone/src/main/Main.jsx
+++ gpt-clone/src/main/Main.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Main.css";
 import { assets } from "../assets/assets.js";
+import { Context } from "../context/Context";
 const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
   return (
     <div className="main">
       <div className="nav">
@@ -9,37 +19,59 @@ const Main = () => {
         <img src={assets.user_icon} alt="" />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello abcd</span>
-          </p>
-          <p>How can I Help You? </p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beautiful places for up coming road trip</p>
-            <img src={assets.compass_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Breafly Summasize this concept : urban planning</p>
-            <img src={assets.bulb_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Brainstorm team bonding activites for our work</p>
-            <img src={assets.message_icon} alt="" />
-          </div>
-          <div className="card">
-            <p>Improve the readablity for the following code</p>
-            <img src={assets.code_icon} alt="" />
-          </div>
-        </div>
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello abcd</span>
+              </p>
+              <p>How can I Help You? </p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful places for up coming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Breafly Summasize this concept : urban planning</p>
+                <img src={assets.bulb_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Brainstorm team bonding activites for our work</p>
+                <img src={assets.message_icon} alt="" />
+              </div>
+              <div className="card">
+                <p>Improve the readablity for the following code</p>
+                <img src={assets.code_icon} alt="" />
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="result">
+              <div className="result-title">
+                <img src={assets.user_icon} alt="" />
+                <p>{recentPrompt}</p>
+              </div>
+              <div className="result-data">
+                <img src={assets.gemini_icon} alt="" />
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              </div>
+            </div>
+          </>
+        )}
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder="Enter your query here..." />
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter your query here..."
+            />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" />
+              <img onClick={() => onSent()} src={assets.send_icon} alt="" />
             </div>
           </div>
           <p className="bottom-info">
