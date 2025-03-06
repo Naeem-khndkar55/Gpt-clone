3034eb8a10dd79efec9723e985908a641ac1d93f
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-03-06 12:18:40 +0600

    Gemini api integrated

---
+++ gpt-clone/src/config/gpt.js
@@ -0,0 +1,36 @@
+/* AIzaSyD7Zpn4V2WK1_OU0s8NCgJnrolyEbVt5P4 */
+// const api_key= "AIzaSyD7Zpn4V2WK1_OU0s8NCgJnrolyEbVt5P4"
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyD7Zpn4V2WK1_OU0s8NCgJnrolyEbVt5P4"
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+  }
+  
+  export default run;
